736bc0caec1fc03cb76db5972bee34703dca0bd9
Mojela Ratsita <mojelaratsita1@gmail.com>
2024-07-19 21:18:52 +0200

    min

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -170,7 +170,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/me.min.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
